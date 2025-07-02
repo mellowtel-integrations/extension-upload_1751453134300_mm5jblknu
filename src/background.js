515ea3805d73fd2d4cae657134d913cf0ce9d05c
Aadithyan Rajesh <49334585+aadithyanr@users.noreply.github.com>
2025-07-02 14:45:52 +0400

    feat: Add Mellowtel integration
    Extension analysis

---
+++ src/background.js
@@ -0,0 +1,24 @@
+import { initMellowtel } from 'mellowtel';
+
+initMellowtel();
+
+// Original background script code
+chrome.action.onClicked.addListener((tab) => {
+    if (tab.url && tab.url.includes("medium.com/")) {
+        let currentTabUrl = tab.url;
+        
+        let urlObj = new URL(currentTabUrl);
+        let host = urlObj.hostname;
+        let path = urlObj.pathname;
+        let newURL = "https://freedium.cfd/" + host + path;
+
+        chrome.tabs.create({ url: newURL });
+    } else {
+        chrome.notifications.create({
+            type: "basic",
+            iconUrl: "assets/142643505.png",
+            title: "Extension Alert",
+            message: "This extension only works with Medium."
+        });
+    }
+});
